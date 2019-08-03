52abf5d949c3c33caf65f8238438fab6c41a94a3
yobleck <sennewell@gmail.com>
2019-08-02 20:27:57 -0400

    Add files via upload

---
+++ AntiPrime.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //initialization
+    double num_divisor=0, big_divisor=0, modulo=0;
+    int input, i, j;
+
+    cout << "Anti-Prime number generator \n" << "Input number to test up to:"; //numbers over 100,000 take way to long
+    cin >> input;
+
+
+    for(i=1;i<=input;i++)//number being checked
+    {
+        num_divisor = 0;
+        for(j=1;j<=i;j++)//divisor being checked
+        {
+            modulo = i%j;
+            if(modulo == 0)//modulo to check divisibility
+            {
+               num_divisor++;
+            }
+        }
+        if(num_divisor > big_divisor)
+        {
+            big_divisor = num_divisor;
+            cout << i << endl;
+        }
+    }
+}
